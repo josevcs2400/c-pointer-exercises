358fb4270be5891966890320b868ad391b17355b
Joe Vasquez <josevwebpro@gmail.com>
2019-03-08 09:26:30 -0700

    global and local arrays

--- global_array.c
+++ global_array.c
@@ -4,9 +4,32 @@
 
 #include <stdio.h>
 
+//1. define a structure
+struct global_struct {
+    int a;
+    char b;
+};
+
+//2. declare a static array of structures outside of a function
+struct global_struct static_array_of_structures[10]; //10 values
+
 void run_global_array() {
+
     printf("Running global array\n");
 
-    // YOUR CODE HERE
+    //an array of letters to use to populate our array of structs
+    char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+
+    //4. assign values in a loop
+    for(int i = 0; i < 10; i++) {
+        static_array_of_structures[i].a = i;
+        static_array_of_structures[i].b = alphabet[i];
+    }
+
+    //print out values
+    for(int i = 0; i < 10; i++) {
+        printf("%d - ", static_array_of_structures[i].a);
+        printf("%c\n", static_array_of_structures[i].b);
+    }
 
 }
--- local_array.c
+++ local_array.c
@@ -7,6 +7,28 @@
 void run_local_array() {
     printf("Running local array\n");
 
-    // YOUR CODE HERE
+    //1. define a structure
+    struct local_struct {
+        int a;
+        char b;
+    };
+
+    //2. declare a static array of structures
+    struct local_struct static_array_of_structs[10];
+
+    //an array of letters to use to populate our array of structs
+    char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+
+    //4. assign values in a loop
+    for(int i = 0; i < 10; i++) {
+        static_array_of_structs[i].a = i;
+        static_array_of_structs[i].b = alphabet[i];
+    }
+
+    //print out values
+    for(int i = 0; i < 10; i++) {
+        printf("%d - ", static_array_of_structs[i].a);
+        printf("%c\n", static_array_of_structs[i].b);
+    }
 
 }
